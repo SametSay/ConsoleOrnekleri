9e369cdf37fa22280b970839e6db7da2d8af4e4a
SametSay <65721610+SametSay@users.noreply.github.com>
2021-01-17 01:47:43 +0300

    Add files via upload

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias
+            
+            string kategoriEtiketi = "Katagori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+            //Console.WriteLine(kategoriEtiketi);
+            //Console.ReadLine();
+        }
+    }
+}
